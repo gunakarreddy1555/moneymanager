2aec56dd8359296c86a412149e13b761cb1cfbe4
gunakarreddy1555 <gunakarreddy15559@gmail.com>
2025-09-17 23:28:58 +0530

    my both Income and expensive repo method updated

--- src/main/java/Gunachattu/moneymanager/repository/IncomeRepository.java
+++ src/main/java/Gunachattu/moneymanager/repository/IncomeRepository.java
@@ -10,19 +10,24 @@ import java.math.BigDecimal;
 import java.time.LocalDate;
 import java.util.List;
 
-public interface IncomeRepository extends JpaRepository<IncomeEntity,Long> {
+public interface IncomeRepository extends JpaRepository<IncomeEntity, Long> {
 
-//    //select * from tbl_incomes where profile_id=? order by date desc
-//    List<IncomeEntity> findByProfileIdOrderByDesc(Long profileId);
-//
-//    //select * from tbl_incomes where profile_id=?1 order by date desc limit 5
-//    List<IncomeEntity> findTop5ProfileIdOrderByDateDesc(Long profileId);
-//
-//    @Query("SELECT SUM(e.amount) FROM IncomeEntity e WHERE  e.profile.id=:profileId")
-//    BigDecimal findTotalExpenseByProfileId(@Param("profileId") Long profileId);
-//
-//    //select * from tbl_incomes where profile_id =?1 and date between ?2 and ?3 and name like %?4% order by date desc
-//    List<IncomeEntity> findByProfileIdAndDateBetweenAndNameContainingIgnoreCase(Long profileId, LocalDate startDate, LocalDate endDate, String keyword , Sort sort);
-//
-//    List<IncomeEntity> findByProfileIdAndDateBetween(Long profileId,LocalDate startDate, LocalDate endDate);
+    // select * from tbl_incomes where profile_id=? order by date desc
+    List<IncomeEntity> findByProfileIdOrderByDateDesc(Long profileId);
+
+    // select * from tbl_incomes where profile_id=? order by date desc limit 5
+    List<IncomeEntity> findTop5ByProfileIdOrderByDateDesc(Long profileId);
+
+    @Query("SELECT SUM(e.amount) FROM IncomeEntity e WHERE e.profile.id = :profileId")
+    BigDecimal findTotalExpenseByProfileId(@Param("profileId") Long profileId);
+
+    // select * from tbl_incomes where profile_id=?1 and date between ?2 and ?3 and name like %?4% order by date desc
+    List<IncomeEntity> findByProfileIdAndDateBetweenAndNameContainingIgnoreCase(
+            Long profileId,
+            LocalDate startDate,
+            LocalDate endDate,
+            String keyword,
+            Sort sort);
+
+    List<IncomeEntity> findByProfileIdAndDateBetween(Long profileId, LocalDate startDate, LocalDate endDate);
 }
